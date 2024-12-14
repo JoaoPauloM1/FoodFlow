b250fe6451e8780aca4e98420929a3ef756fb894
João Paulo Maciel Borges <joaopaulomacielborges@gmail.com>
2024-12-14 12:59:26 -0300

    teste 1

--- backend/app.py
+++ backend/app.py
@@ -1,10 +1,10 @@
-from flask import Flask, jsonify
+from flask import Flask
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return jsonify({"mensagem": "Backend funcionando!"})
+    return "Bem-vindo ao FoodFlow!"
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    app.run(debug=True)
