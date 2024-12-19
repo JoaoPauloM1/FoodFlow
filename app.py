ca458a533474abc3e4cc1bf3f4189cc9a6314e13
João Paulo Maciel Borges <joaopaulomacielborges@gmail.com>
2024-12-19 19:12:19 -0300

    Ultima correção backend

--- app.py
+++ app.py
@@ -3,27 +3,18 @@ import requests
 
 app = Flask(__name__)
 
-@app.route('/restaurantes', methods=['GET'])
-def restaurantes():
-    try:
-        url = 'https://joaopaulom1.github.io/FoodFlow/db.json'
-        response = requests.get(url)
-
-        # Verifique se a resposta foi bem-sucedida
-        if response.status_code == 200:
-            dados = response.json()  # Converte a resposta para JSON
-            return jsonify(dados)  # Retorna os dados como JSON
-        else:
-            return jsonify({"error": "Erro ao acessar o arquivo JSON"}), 500
-    except Exception as e:
-        # Caso ocorra um erro na requisição ou outro erro no servidor
-        return jsonify({"error": str(e)}), 500
-
 @app.route('/')
 def home():
     return "Backend do FoodFlow!"
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    app.run(debug=True)
+# URL Backend: https://foodflow-nvcj.onrender.com
 
-restaurantes()
+# Pegar os restaurantes
+@app.route('/restaurantes', methods=['GET'])
+def get_restaurants():
+    url = 'https://joaopaulom1.github.io/FoodFlow/db.json'
+    response = requests.get(url)
+    dados = response.json()
+get_restaurants()
